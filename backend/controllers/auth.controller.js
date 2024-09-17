8819dbb9e1a19d354c8dde6eaa1838abf13bbad1
MbusyaNgesa <theengesa@gmail.com>
2024-09-17 16:48:55 +0300

    SignUp Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,23 +1,28 @@
 import { User } from "../models/user.model.js";
-import { generateVerificationToken } from "../utils/generateVerificationToken.js";
+import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookies } from "../utils/generateTokenAndSetCookies.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
+
   try {
     if (!email || !password || !name) {
       throw new Error("All fields are required");
     }
 
     const userAlreadyExists = await User.findOne({ email });
+    console.log("userAlreadyExists", userAlreadyExists);
+
     if (userAlreadyExists) {
       return res
         .status(400)
         .json({ success: false, message: "User already exists" });
     }
-    console.log("Ngesa");
 
-    const hashedPassword = await bcryptjs.hash(password, 10);
-    const verificationToken = generateVerificationToken();
+    const hashedPassword = await bcryptjs.hash(password, 6);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
     const user = new User({
       email,
       password: hashedPassword,
@@ -27,6 +32,18 @@ export const signup = async (req, res) => {
     });
 
     await user.save();
+
+    //jwt
+    generateTokenAndSetCookies(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created Succesfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import authroutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 //For Testing
 // app.get("/", (req, res) => {
