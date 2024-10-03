11ed3db9a41cc18e4df99ee4b5d5a691b3280a5a
MbusyaNgesa <theengesa@gmail.com>
2024-10-03 16:24:45 +0300

    Login & Logout Endpoints

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -86,9 +86,37 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.send("logout Route");
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
 
 export const login = async (req, res) => {
-  res.send("login Route");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: true, message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: true, message: "Invalid credentials" });
+    }
+    generateTokenAndSetCookies(res, user._id);
+    user.lastLogin = new Date();
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: { ...user._doc, password: undefined },
+    });
+  } catch (error) {
+    console.log("Error in Login", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
