e36348baa8ed311148a2ef768bf4f5045691b139
MbusyaNgesa <theengesa@gmail.com>
2024-11-03 17:15:51 +0300

    React Setup and Animation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FloatingShape } from "./components";
+
+const App = () => {
+  return (
+    <div
+      className="min-h-screen bg-gradient-to-br 
+    from-blue-950 via-cyan-700 to-blue-600 flex 
+    items-center justify-center relative overflow-hidden "
+    >
+      <FloatingShape
+        color="bg-blue-300"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-orange-200"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-indigo-400"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,1 @@
+export { FloatingShape } from "./FloatingShape";
